98fa49e08eb078919323331e0be322bd0bc645cf
Sunthorn Ruckani <sunthorn.rk@gamil.com>
2024-08-07 13:35:07 +0700

    last chang api url to render backend url

--- utils/api.js
+++ utils/api.js
@@ -1,16 +1,18 @@
 import {API_URL, STRAPI_API_TOKEN} from "./urls";
 
 export const fetchDataFromApi = async (endpoint) => {
-  const options = {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + STRAPI_API_TOKEN,
-    },
-  };
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     Authorization: "Bearer " + STRAPI_API_TOKEN,
+  //   },
+  // };
 
-  const res = await fetch(`${API_URL}${endpoint}`, options);
+  const res = await fetch(`${API_URL}${endpoint}`);
+  // const res = await fetch(`${API_URL}${endpoint}`, options);
   const data = await res.json();
 
+  console.log(res);
   return data;
 };
 
